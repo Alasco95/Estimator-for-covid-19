f4a7967053f4634e33303eee8f58fe74866a0544
Alasco95 <allahyaj@gmail.com>
2020-04-14 04:21:38 +0100

    challenge

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,13 @@
 const covid19ImpactEstimator = () => data => {
     const impact = {};
     const severeImpact = {};
-
-    const input = data;
-    impact.currentlyInfected = input.reportedCases * 10;
-    severeImpact.currentlyInfected = input.reportedCases * 50;
-    impact.infectionByRequestedTime = impact.currentlyInfected * (2**9);
-    severeImpact.infectionByRequestedTime = severeImpact.currentlyInfected * (2 ** 9);
-    return {  
-      data: {},          // the input data you got   
+   const input = data;
+  impact.currentlyInfected = input.reportedCases * 10;
+  severeImpact.currentlyInfected = input.reportedCases * 50;
+  impact.infectionByRequestedTime = impact.currentlyInfected * (2**9);
+  severeImpact.infectionByRequestedTime = severeImpact.currentlyInfected * (2 ** 9);
+  return { 
+  data: {},          // the input data you got   
        impact: {},        // your best case estimation   
          severeImpact: {}   // your severe case estimation
          } ;
